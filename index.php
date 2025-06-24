994d8333731cf7a8a776fff9c39ee3219430ba10
SimonDEPRUN <160472926+SimonDEPRUN@users.noreply.github.com>
2025-06-24 14:46:07 +0200

    inclusion des fichier partiel et include_once

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include "./dependencies.php" ?>
+    <?php require "./_dependencies.php" ?>
     <title>Home page</title>
 </head>
 
 <body>
-    <?php include "./_menu.php" ?>
+    <?php include_once "./_menu.php" ?>
+
     <main>
         <h1 class="text-primary">Page d'accueil</h1>
         <p>Bienvenue dans notre application <strong>Web php</strong></p>
--- inscription.php
+++ inscription.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include "./dependencies.php" ?>
+    <?php require "./_dependencies.php" ?>
     <title>Inscription</title>
 </head>
 
